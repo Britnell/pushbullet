007f261ffdbd72d6e0dd9684ecb1e1b25ed7019f
Britnell <tommy.britnell@gmail.com>
2024-02-02 20:43:51 +0100

    remove pwa adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import AstroPWA from "@vite-pwa/astro";
+// import AstroPWA from "@vite-pwa/astro";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [react(), tailwind(), AstroPWA()],
+  integrations: [react(), tailwind()],
 });

c6dcb870946dd477fc6914720e9ffdd8f8446db7
Britnell <tommy.britnell@gmail.com>
2024-02-02 17:45:14 +0100

    adding PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import AstroPWA from "@vite-pwa/astro";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node({
-    mode: "standalone"
+    mode: "standalone",
   }),
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), AstroPWA()],
 });

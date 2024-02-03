9f6ae6413e361d64b8112170d73af4d0dcbe9ea7
Britnell <tommy.britnell@gmail.com>
2024-02-03 16:44:32 +0100

    vanilla.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="astro/client" />
 /// <reference types="vite-plugin-pwa/info.d.ts" />
 /// <reference types="vite-plugin-pwa/client.d.ts" />
-/// <reference types="vite-plugin-pwa/register" />
+/// <reference types="vite-plugin-pwa/vanilla.d.ts" />

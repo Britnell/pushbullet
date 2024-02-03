8612dad8a8435d96efca0ab8cb8d0109a88f4382
Britnell <tommy.britnell@gmail.com>
2024-02-03 15:09:14 +0100

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 // import AstroPWA from "@vite-pwa/astro";
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: netlify(),
   integrations: [react(), tailwind()],
 });

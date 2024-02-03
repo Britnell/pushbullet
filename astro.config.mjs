ceda78df59eb9018d9c06afcb10cdbbc55460362
Britnell <tommy.britnell@gmail.com>
2024-02-03 15:33:16 +0100

    order

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import AstroPWA from "@vite-pwa/astro";
-
 import netlify from "@astrojs/netlify";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({

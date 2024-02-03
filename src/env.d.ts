8eff7820ff554f034ab9b3e2c37d3b055f2ce4a0
Britnell <tommy.britnell@gmail.com>
2024-02-03 16:46:26 +0100

    liek ref

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,3 @@
 /// <reference types="astro/client" />
-/// <reference types="vite-plugin-pwa/info.d.ts" />
-/// <reference types="vite-plugin-pwa/client.d.ts" />
-/// <reference types="vite-plugin-pwa/vanilla.d.ts" />
+/// <reference types="vite-plugin-pwa/info" />
+/// <reference types="vite-plugin-pwa/vanillajs" />

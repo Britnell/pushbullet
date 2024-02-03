4281d989811f54a91a353f3fe8f59e4a7aa6cc93
Britnell <tommy.britnell@gmail.com>
2024-02-03 18:40:52 +0100

    style login page

--- src/comp/AppBits.tsx
+++ src/comp/AppBits.tsx
@@ -32,74 +32,76 @@ function AppBits() {
   let lastDate = "";
 
   return (
-    <div className=" h-[calc(100dvh-48px)] flex flex-col ">
-      <div ref={scrollRef} className=" grow  overflow-auto">
-        {query.data && (
-          <ul>
-            {query.data.map((bit) => {
-              // Render dates
-              const date = bit.date.split(" ")[0];
-              const showDate = date !== lastDate;
-              lastDate = date;
-              return (
-                <Fragment key={bit.id}>
-                  {showDate && (
-                    <p className="  mt-10 mb-4 text-gray-500 text-center  border-b-2 border-gray-300 pb-2">
-                      {date}
-                    </p>
-                  )}
-                  <div className="mb-4 group flex items-center  ">
-                    <li className="  bg-blue-200 p-2 pl-6 pr-6 rounded-xl w-fit">
-                      <RenderHyperlinks text={bit.text} />{" "}
-                      <span className=" ml-6 text-[0.7em]">
-                        {bit.date.slice(-8)}
-                      </span>
-                    </li>
-                    <button
-                      className=" ml-6  bg-slate-100 hidden group-hover:block w-8 h-8 rounded-full "
-                      onClick={() => deletor.mutate(bit.id)}
-                    >
-                      x
-                    </button>
-                  </div>
-                </Fragment>
-              );
-            })}
-          </ul>
-        )}
-        {query.error && (
-          <div>
-            <p>ERROR - {query.error}</p>
-          </div>
-        )}
-      </div>
-      <form
-        onSubmit={(ev) => {
-          ev.preventDefault();
-          submit();
-        }}
-      >
-        <div className="shadowclip p-1 flex flex-wrap items-start gap-3 shadow-[0px_0px_10px_rgba(0,0,0,0.3)] clip">
-          <textarea
-            className="  rounded-sm px-2 py-1 w-full max-w-[600px]"
-            name="text"
-            rows={3}
-            placeholder="Your new Bit..."
-            value={input}
-            onChange={(ev) => setInput(ev.target.value)}
-            onKeyDown={(ev) => {
-              if (ev.code === "Enter" && !ev.shiftKey) {
-                ev.preventDefault();
-                submit();
-              }
-            }}
-          />
-          <button className=" bg-blue-200 rounded-full px-4 py-1">
-            Submit
-          </button>
+    <main class="mx-auto max-w-[900px] px-6 sm:px-8">
+      <div className=" h-[calc(100dvh-48px)] flex flex-col ">
+        <div ref={scrollRef} className=" grow  overflow-auto">
+          {query.data && (
+            <ul>
+              {query.data.map((bit) => {
+                // Render dates
+                const date = bit.date.split(" ")[0];
+                const showDate = date !== lastDate;
+                lastDate = date;
+                return (
+                  <Fragment key={bit.id}>
+                    {showDate && (
+                      <p className="  mt-10 mb-4 text-gray-500 text-center  border-b-2 border-gray-300 pb-2">
+                        {date}
+                      </p>
+                    )}
+                    <div className="mb-4 group flex items-center  ">
+                      <li className="  bg-blue-200 p-2 pl-6 pr-6 rounded-xl w-fit">
+                        <RenderHyperlinks text={bit.text} />{" "}
+                        <span className=" ml-6 text-[0.7em]">
+                          {bit.date.slice(-8)}
+                        </span>
+                      </li>
+                      <button
+                        className=" ml-6  bg-slate-100 hidden group-hover:block w-8 h-8 rounded-full "
+                        onClick={() => deletor.mutate(bit.id)}
+                      >
+                        x
+                      </button>
+                    </div>
+                  </Fragment>
+                );
+              })}
+            </ul>
+          )}
+          {query.error && (
+            <div>
+              <p>ERROR - {query.error}</p>
+            </div>
+          )}
         </div>
-      </form>
-    </div>
+        <form
+          onSubmit={(ev) => {
+            ev.preventDefault();
+            submit();
+          }}
+        >
+          <div className="shadowclip p-1 flex flex-wrap items-start gap-3 shadow-[0px_0px_10px_rgba(0,0,0,0.3)] clip">
+            <textarea
+              className="  rounded-sm px-2 py-1 w-full max-w-[600px]"
+              name="text"
+              rows={3}
+              placeholder="Your new Bit..."
+              value={input}
+              onChange={(ev) => setInput(ev.target.value)}
+              onKeyDown={(ev) => {
+                if (ev.code === "Enter" && !ev.shiftKey) {
+                  ev.preventDefault();
+                  submit();
+                }
+              }}
+            />
+            <button className=" bg-blue-200 rounded-full px-4 py-1">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </main>
   );
 }
 

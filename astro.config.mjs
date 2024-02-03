45e6cd62d8cdc1ec355acac05046876e693086c5
Britnell <tommy.britnell@gmail.com>
2024-02-03 15:20:14 +0100

    add pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-// import vercel from "@astrojs/vercel/serverless";
+import AstroPWA from "@vite-pwa/astro";
 
 import netlify from "@astrojs/netlify";
 
@@ -9,5 +9,5 @@ import netlify from "@astrojs/netlify";
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), AstroPWA()],
 });

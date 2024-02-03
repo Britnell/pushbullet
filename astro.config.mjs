04dbc7d5e40b62386c8e8638d2c539aca0d8780a
Britnell <tommy.britnell@gmail.com>
2024-02-03 16:28:48 +0100

    astro vite-pwa !?

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,55 @@ import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 import AstroPWA from "@vite-pwa/astro";
 
+const manifest = {
+  name: "Astro PWA",
+  short_name: "Astro PWA",
+  theme_color: "#ffffff",
+  icons: [
+    {
+      src: "pwa-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      src: "pwa-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+    },
+    {
+      src: "pwa-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+      purpose: "any maskable",
+    },
+  ],
+};
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [react(), tailwind(), AstroPWA()],
+  integrations: [
+    react(),
+    tailwind(),
+    AstroPWA({
+      mode: "development",
+      base: "/",
+      scope: "/",
+      includeAssets: ["favicon.svg"],
+      registerType: "autoUpdate",
+      manifest,
+      workbox: {
+        navigateFallback: "/",
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\//],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
+    }),
+  ],
 });
